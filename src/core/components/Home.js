167cbaaa91be8446a4098fa5f050b39f69414643
mmassetti <matiasmassetti@gmail.com>
2020-07-11 16:35:28 -0300

    change home view

--- src/core/components/Home.js
+++ src/core/components/Home.js
@@ -1,9 +1,23 @@
-import {Container, Content, Spinner} from 'native-base';
+import {
+  Container,
+  Content,
+  Spinner,
+  Header,
+  Card,
+  CardItem,
+  Text,
+  Icon,
+  Right,
+  Thumbnail,
+  Button,
+  Left,
+  Body,
+} from 'native-base';
+
 import React, {Component} from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Image} from 'react-native';
 import {mainThemeColor, homeButtons} from '../../configuration';
 import {HomeCard, ImagePickerService, ImageProcessor} from '../../shared';
-import {ImageModel} from '../../shared/models/ImageModel.js';
 
 export class Home extends Component {
   picker;
@@ -18,15 +32,38 @@ export class Home extends Component {
     };
   }
 
+  goToItem() {
+    alert('llevame a la vista de la sesion por favor');
+  }
+
   render() {
     return (
       <Container>
-        <Content contentContainerStyle={styles.container}>
-          {this.state.loading ? (
-            <Spinner color={mainThemeColor(1)} />
-          ) : (
-            <>{this.getHomeButtons()}</>
-          )}
+        <Header />
+        <Content>
+          <Card>
+            <CardItem button onPress={this.goToItem}>
+              <Icon active name="logo-googleplus" />
+              <Text>Google Plus</Text>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </CardItem>
+            <CardItem>
+              <Icon active name="logo-googleplus" />
+              <Text>Google Plus</Text>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </CardItem>
+            <CardItem>
+              <Icon active name="logo-googleplus" />
+              <Text>Google Plus</Text>
+              <Right>
+                <Icon name="arrow-forward" />
+              </Right>
+            </CardItem>
+          </Card>
         </Content>
       </Container>
     );
@@ -41,47 +78,6 @@ export class Home extends Component {
         text={button.text}
       />
     ));
-  }
-
-  launch = picker => async () => {
-    this.setState({loading: true});
-    try {
-      const {uri, data, width, height} = await this.picker[
-        'getImageFrom' + picker
-      ]();
-      let imgModel = new ImageModel(data, height, width, uri);
-      this.routeToImageView(imgModel);
-    } catch (error) {
-      this.setState({loading: false});
-    }
-  };
-
-  async routeToImageView(originalImgModel) {
-    const now = Date.now();
-    const {
-      img,
-      //TODO: Encapsulate percentages in one object
-      percentageGreen,
-      percentageYellow,
-      percentageNaked,
-    } = await this.imageProcessor.processImage(originalImgModel.uri);
-    console.log('Elapsed time: ', Date.now() - now, 'ms');
-    this.setState({
-      loading: false,
-    });
-
-    this.props.navigation.navigate('Imagen', {
-      originalImage: originalImgModel,
-      processedImage: new ImageModel(
-        img,
-        originalImgModel.height,
-        originalImgModel.width,
-      ),
-      shouldRotate: originalImgModel.width < originalImgModel.height,
-      percentageGreen: percentageGreen,
-      percentageYellow: percentageYellow,
-      percentageNaked: percentageNaked,
-    });
   }
 }
 
