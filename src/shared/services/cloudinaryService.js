77c33e16d3b4fc7cbe858b908fa44bea5b022010
Juan Jouglard <jouglardjuan@gmail.com>
2020-07-12 15:05:46 -0300

    Connection with cloudinary - Signed upload

--- src/shared/services/cloudinaryService.js
+++ src/shared/services/cloudinaryService.js
@@ -1,4 +1,5 @@
-import {CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET} from '@env';
+import {CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_NAME} from '@env';
+import {sha1} from 'react-native-sha256';
 
 export class CloudinaryService {
   static _instance;
@@ -8,15 +9,37 @@ export class CloudinaryService {
     return this.instance;
   }
 
-  uploadPhoto(photo) {
+  async uploadPhoto(photo) {
     const data = new FormData();
+    const timestamp = Date.now();
 
     data.append('file', photo);
-    data.append('upload_preset', 'intaapp');
-    data.append('cloud_name', 'proyectointauns');
+    data.append('cloud_name', CLOUDINARY_NAME);
+    data.append('timestamp', timestamp);
+    data.append('api_key', CLOUDINARY_API_KEY);
+    data.append(
+      'signature',
+      await this.getSignature(timestamp, CLOUDINARY_API_SECRET),
+    );
+
     fetch('https://api.cloudinary.com/v1_1/proyectointauns/image/upload', {
-      method: 'PUT',
+      method: 'POST',
       body: data,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'multipart/form-data',
+      },
+    }).then(console.log);
+  }
+
+  getSignature(timestamp, secret) {
+    const plainText = `timestamp=${timestamp}${secret}`;
+    return sha1(plainText);
+  }
+
+  getImageByID(id) {
+    fetch('https://api.cloudinary.com/v1_1/proyectointauns/resources/images', {
+      method: 'GET',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'multipart/form-data',
