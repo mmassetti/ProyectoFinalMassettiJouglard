86a92759260a2f519e43880b20e679a7121eb8d3
mmassetti <matiasmassetti@gmail.com>
2020-07-13 21:50:15 -0300

    Sessions with search

--- src/core/components/SessionsList.js
+++ src/core/components/SessionsList.js
@@ -1,17 +1,30 @@
-import React, { useState, useEffect} from 'react';
-import {StyleSheet, FlatList} from 'react-native';
-import {withFirebase} from '../../shared';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, FlatList } from "react-native";
+import { withFirebase } from "../../shared";
+import SearchInput, { createFilter } from "react-native-search-filter";
 
-function SessionsList({firebaseService}) {
+function SessionsList({ firebaseService }) {
   const [sessions, setSessions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(async () => {
     const sessions = await firebaseService.getSessions();
     setSessions(sessions);
     return null;
   });
-  return <FlatList data={sessions} renderItem={}/>;
+  const filteredSession = sessions.filter(
+    createFilter(searchTerm, ['user', 'description'])
+  );
+  return (
+    <>
+      <SearchInput
+        onChangeText={setSearchTerm}
+        style={styles.searchInput}
+        placeholder="Buscar por nombre, descripcion, mes ..."
+      />
+      <FlatList data={filteredSession} renderItem={} />
+    </>
+  );
 }
 
 export default withFirebase(SessionsList);
-
