fa84fd7649b790898efb4f6176e533d8f656e162
mmassetti <matiasmassetti@gmail.com>
2020-08-11 19:31:22 -0300

    working with date

--- src/core/components/NewSession.js
+++ src/core/components/NewSession.js
@@ -1,12 +1,39 @@
 //@ts-check
 import React, {useState, useEffect} from 'react';
 import {StyleSheet} from 'react-native';
-import {Text} from 'react-native';
+import {Container, Header, Content, DatePicker, Text} from 'native-base';
 
 export default function NewSession(props) {
-  useEffect(() => {}, []);
+  let [chosenDate, setChosenDate] = useState(new Date());
 
-  return <Text> Nueva sesion</Text>;
+  useEffect(() => {}, [chosenDate]);
+
+  function setDate(newDate) {
+    setChosenDate(newDate);
+  }
+
+  return (
+    <Container>
+      <Content>
+        <DatePicker
+          defaultDate={new Date(2018, 4, 4)}
+          minimumDate={new Date(2018, 1, 1)}
+          // maximumDate={new Date(2018, 12, 31)}
+          locale={'es'}
+          timeZoneOffsetInMinutes={undefined}
+          modalTransparent={false}
+          animationType={'fade'}
+          androidMode={'default'}
+          placeHolderText="Seleccionar fecha"
+          textStyle={{color: 'green'}}
+          placeHolderTextStyle={{color: '#d3d3d3'}}
+          onDateChange={() => setDate}
+          disabled={false}
+        />
+        <Text>Date: {chosenDate.toString().substr(4, 12)}</Text>
+      </Content>
+    </Container>
+  );
 }
 
 const styles = StyleSheet.create({});
