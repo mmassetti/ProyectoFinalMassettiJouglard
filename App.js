7c900fe09b4d91c3ee278d6814a322518b8188f9
Juan Jouglard <jouglardjuan@gmail.com>
2020-08-13 15:23:19 -0300

    Spinner logic

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import AsyncStorage from '@react-native-community/async-storage';
-import {createStackNavigator} from '@react-navigation/stack';
 import React, {useEffect, useReducer, useState} from 'react';
-import {StyleSheet} from 'react-native';
 import Main from './src/core/components/Main';
 import StateContext from './src/core/context/StateContext';
 import stateReducer from './src/core/reducers/stateReducer';
 import {Tour} from './src/guided-tour/Tour';
 
-const Stack = createStackNavigator();
 function App() {
-  const [state, dispatcher] = useReducer(stateReducer, {});
+  const [state, dispatcher] = useReducer(stateReducer[0], stateReducer[1]);
   const [showRealApp, setShowApp] = useState(false);
 
   useEffect(async () => {
@@ -37,13 +34,5 @@ function App() {
     }
   }
 }
-
-const styles = StyleSheet.create({
-  menuicon: {
-    color: '#f5f7f7',
-    fontSize: 24,
-    marginRight: 20,
-  },
-});
 
 export default App;
--- src/core/reducers/stateReducer.js
+++ src/core/reducers/stateReducer.js
@@ -1,9 +1,27 @@
-export default function stateReducer(state, action) {
-  switch (action.type) {
-    case 'ADD_USER':
-      return state;
+import combineReducers from 'react-combine-reducers';
 
+function spinner(state, action) {
+  switch (action.type) {
+    case 'SHOW_SPINNER':
+      return true;
+    case 'HIDE_SPINNER':
+      return false;
     default:
       return state;
   }
 }
+
+function showApp(state, action) {
+  switch (action.type) {
+    case 'SHOW_APP':
+      return true;
+    case 'HIDE_APP':
+      return false;
+    default:
+      return state;
+  }
+}
+export default combineReducers({
+  spinner: [spinner, false],
+  showApp: [showApp, false],
+});
