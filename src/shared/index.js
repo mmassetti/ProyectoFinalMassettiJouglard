78f406b498478d73cd43bba12b75d73a97ffd4bb
Juan Jouglard <jouglardjuan@gmail.com>
2020-09-06 18:12:51 -0300

    New Service injector

--- src/shared/index.js
+++ src/shared/index.js
@@ -4,17 +4,15 @@ export {HomeCard} from './components/HomeCard';
 export {Percentage} from './components/PercentageCircle';
 export {CustomImage} from './components/CustomNativeImage';
 export {ImageWithAdjustment} from './components/ImageWithButton';
-export {withCloudinary} from './components/HOCForInjection/WithCloudinary';
-export {withFirebase} from './components/HOCForInjection/WithFirebase';
-export {withImagePicker} from './components/HOCForInjection/WithImagePicker';
 export {
-  withImageProcessing,
-} from './components/HOCForInjection/WithImageProcessing';
-export {Separator} from './components/ListSeparator';
-export {
+  withFirebase,
+  withCloudinary,
+  withAlertService,
   withSessionsService,
-} from './components/HOCForInjection/WithSessionService';
-export {withAlertService} from './components/HOCForInjection/WithAlertService';
+  withImageProcessing,
+  withImagePicker,
+} from './components/HOCForInjection/WithService';
+export {Separator} from './components/ListSeparator';
 export {AddLote} from './components/AddLote';
 export {uuidv4 as uniqueId} from './services/uuidService';
 export {NetStatusBar} from './components/NetStatusBar';
