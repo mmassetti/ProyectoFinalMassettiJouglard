731e08672a3ae5f542069c4f16377698018666d9
mmassetti <matiasmassetti@gmail.com>
2020-11-07 20:04:10 -0300

    adding recommendations tuto

--- src/configuration/slides.js
+++ src/configuration/slides.js
@@ -3,7 +3,7 @@ export const slides = [
     key: 's1',
     title: 'Bienvenidos',
     text:
-      'Esta app le ayudará a determinar el porcentaje de cubrimiento que presenta un suelo.',
+      'Esta app te ayudará a determinar el porcentaje de cubrimiento que presenta un suelo.',
     image: require('../guided-tour/assets/Logo.png'),
     backgroundColor: '#3395ff',
   },
@@ -11,7 +11,7 @@ export const slides = [
     key: 's2',
     title: 'Sesiones',
     text:
-      'En la pantalla principal encontrará todas las sesiones creadas. Podrá buscar sesiones por nombre o usuario. También podrá crear una nueva sesión.',
+      'En la pantalla principal encontrarás todas las sesiones creadas. Podrás buscar sesiones por nombre o usuario. También podrás crear una nueva sesión.',
     image: require('../guided-tour/assets/Sessions.jpeg'),
     backgroundColor: '#DB6C79',
   },
@@ -19,7 +19,7 @@ export const slides = [
     key: 's3',
     title: 'Detalle de la sesión',
     text:
-      'Al ingresar a una de las sesiones podrá ver sus detalles, lotes y notas.',
+      'Al ingresar a una de las sesiones podrás ver sus detalles, lotes y notas.',
     image: require('../guided-tour/assets/SessionDetails.jpeg'),
     backgroundColor: '#DB6C79',
   },
@@ -27,7 +27,7 @@ export const slides = [
     key: 's4',
     title: 'Lote',
     text:
-      'Todas las sesiones tienen lotes. Al ingresar a alguno de ellos podrá ver las imágenes y pasturas asociadas a dicho lote, como así también su promedio.',
+      'Todas las sesiones tienen lotes. Al ingresar a alguno de ellos podrás ver las imágenes y pasturas asociadas a dicho lote, como así también su promedio.',
     image: require('../guided-tour/assets/LoteDetails.jpeg'),
     backgroundColor: '#DB6C79',
   },
@@ -35,14 +35,15 @@ export const slides = [
     key: 's5',
     title: 'Cargar una imágen',
     text:
-      'En cada lote o pastura, podrá seleccionar una imagen desde la galería o tomar una foto en el momento utilizando la cámara.',
+      'En cada lote o pastura, podrás seleccionar una imágen desde la galería o tomar una foto en el momento utilizando la cámara.',
     image: require('../guided-tour/assets/LoadImage.jpeg'),
     backgroundColor: '#DB6C79',
   },
   {
     key: 's6',
-    title: 'Recomendaciones',
-    text: 'Poner recomendaciones a la hora de sacar la foto',
+    title: 'Recomendaciones al sacar la foto',
+    text:
+      '¡Siguiendo estos consejos la comparación entre imágenes será más precisa!',
     image: require('../guided-tour/assets/Recommendations.png'),
     backgroundColor: '#DB6C79',
   },
@@ -50,7 +51,7 @@ export const slides = [
     key: 's7',
     title: 'Porcentajes',
     text:
-      'Obtendrá el porcentaje de suelo que este cubierto con vegetación viva, seca o se encuentre sin cubrir. Además, podrá hacer zoom, retocar la imágen y agregar una nota.',
+      'Obtendrás el porcentaje de suelo que este cubierto con vegetación viva, seca o se encuentre sin cubrir. Además, podrás hacer zoom, retocar la imágen y agregar anotaciones.',
     image: require('../guided-tour/assets/ImageResultsZoomAndEdit.jpeg'),
     backgroundColor: '#DB6C79',
   },
@@ -58,7 +59,7 @@ export const slides = [
     key: 's8',
     title: 'Imágen procesada',
     text:
-      'Aquí podrá observar que partes de la imágen han sido detectadas como vegetación. Los sectores que aparecen en negro son detectados como suelo desnudo. También podrá observar la imágen original.',
+      'Aquí podrás observar que partes de la imágen han sido detectadas como vegetación. Los sectores que aparecen en negro son detectados como suelo desnudo. También podrás observar la imágen original.',
     image: require('../guided-tour/assets/ProcessedImage.png'),
     backgroundColor: '#DB6C79',
   },
@@ -66,7 +67,7 @@ export const slides = [
     key: 's9',
     title: 'Ajustes',
     text:
-      'Aquí podrá modificar el brillo, saturación y contraste de la imágen, con el fin de poder mejorarla. Luego de presionar "Confirmar", se realizará nuevamente el procesamiento.',
+      'En los ajustes podrás modificar el brillo, saturación y contraste de la imágen, con el fin de poder mejorarla. Luego de presionar "Confirmar", se realizará nuevamente el procesamiento.',
     image: require('../guided-tour/assets/Adjustment.jpg'),
     backgroundColor: '#DB6C79',
   },
@@ -74,7 +75,7 @@ export const slides = [
     key: 's10',
     title: 'Antes y después',
     text:
-      'Para cada imágen tendrá la posibilidad de cargar otra luego de, por ejemplo, hacer un recorte de la vegetación. De esta forma podrá llevar registro de la diferencia antes y despúes del recorte',
+      'Para cada imágen tendrás la posibilidad de cargar otra luego de, por ejemplo, hacer un recorte de la vegetación. De esta forma podrás llevar registro de la diferencia antes y despúes del recorte',
     image: require('../guided-tour/assets/BeforeAfter.jpeg'),
     backgroundColor: '#DB6C79',
   },
@@ -83,7 +84,7 @@ export const slides = [
     key: 's11',
     title: 'Reciente',
     text:
-      'En esta pantalla estarán los lotes que fueron accedidos recientemente.',
+      'En esta pantalla estarán los últimos diez lotes que fueron accedidos recientemente.',
     image: require('../guided-tour/assets/RecentLotes.jpeg'),
     backgroundColor: '#FEBE29',
   },
@@ -91,7 +92,7 @@ export const slides = [
     key: 's12',
     title: 'Proceso rápido',
     text:
-      'Por último, podrá hacer un análisis rápido de la cobertura del suelo, sin necesidad de crear una sesión. En este caso no se guardarán los resultados del procesamiento.',
+      'Por último, podrás hacer un análisis rápido de la cobertura del suelo, sin necesidad de crear una sesión. En este caso no se guardarán los resultados del procesamiento.',
     image: require('../guided-tour/assets/FastProcess.jpeg'),
     backgroundColor: '#22bcb5',
   },
@@ -99,7 +100,7 @@ export const slides = [
     key: 's13',
     title: 'Página web',
     text:
-      'Creamos una web desde donde podrá ver los resultados de las sesiones, editar información y descargar informes en formato CSV y PDF. Puede visitarla en: LINK A LA WEB',
+      'Creamos una web desde donde podrás ver los resultados de las sesiones, editar información y descargar informes en formato CSV y PDF. Podes visitarla en: LINK A LA WEB',
     image: require('../guided-tour/assets/WebHome.jpeg'),
     backgroundColor: '#3395ff',
   },
